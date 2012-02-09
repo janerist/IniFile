7d71e2fcf3c78a89ac99e0ad72cc538cba92872c
janerist <jan.erik.strom@gmail.com>
2012-02-09 23:30:51 +0100

    inital commit

---
+++ IniFileTests.cs
@@ -0,0 +1,121 @@
+using System.IO;
+using NUnit.Framework;
+
+namespace YourNamespace
+{
+    [TestFixture]
+    public class IniFileTests
+    {
+        [Test]
+        public void TestLoad()
+        {
+            var iniFile = new IniFile(new StringReader("[Foo]\nbar=1\nbaz=qux\n[Lol]\nrofl = copter"));
+            Assert.That(iniFile.Section("Foo").Get("bar"), Is.EqualTo("1"));
+            Assert.That(iniFile.Section("Foo").Get("baz"), Is.EqualTo("qux"));
+
+            Assert.That(iniFile.Section("Lol").Get("rofl"), Is.EqualTo("copter"));
+        }
+
+        [Test]
+        public void TestCreatesSection()
+        {
+            var iniFile = new IniFile(new StringReader("[Foo]\nbar=1\nbaz=qux"));
+            var category = iniFile.Section("foo");
+            Assert.That(category, Is.Not.Null);
+            Assert.That(category.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestKeyNotFound()
+        {
+            var iniFile = new IniFile(new StringReader("[Foo]\nbar=1\nbaz=qux"));
+            Assert.That(iniFile.Section("Foo").Get("nope"), Is.Null);
+        }
+
+        [Test]
+        public void TestOverwrites()
+        {
+            var iniFile = new IniFile(new StringReader("[Foo]\ndupe=1\ndupe=qux"));
+            Assert.That(iniFile.Section("Foo").Get("dupe"), Is.EqualTo("qux"));
+        }
+
+        [Test]
+        public void TestSkipsComments()
+        {
+            var iniFile = new IniFile(new StringReader("[Foo]\n;comment\nbar=1\nbaz=qux"));
+            Assert.That(iniFile.Sections().Length, Is.EqualTo(1));
+            Assert.That(iniFile.Sections()[0].Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void TestMergesSectionsWithSameName()
+        {
+            var iniFile = new IniFile(new StringReader("[Foo]\nbar=1\nbaz=qux\n[Foo]\nyes=true\n[Foo2]\nno=false"));
+            Assert.That(iniFile.Sections().Length, Is.EqualTo(2));
+            Assert.That(iniFile.Sections()[0].Count, Is.EqualTo(3));
+            Assert.That(iniFile.Section("Foo").Get("yes"), Is.EqualTo("true"));
+        }
+
+        [Test]
+        public void TestRemoveSection()
+        {
+            var iniFile = new IniFile(new StringReader("[Foo]\nbar=1\nbaz=qux\n[Foo]\nyes=true\n[Foo2]\nno=false"));
+            iniFile.RemoveSection("Foo2");
+            Assert.That(iniFile.Sections().Length, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TestRemoveProperty()
+        {
+            var iniFile = new IniFile(new StringReader("[Foo]\nbar=1\nbaz=qux\n[Foo]\nyes=true\n[Foo2]\nno=false"));
+
+            Assert.That(iniFile.Section("Foo").Get("baz"), Is.Not.Null);
+            iniFile.Section("Foo").Set("baz", null);
+            Assert.That(iniFile.Section("Foo").Get("baz"), Is.Null);
+
+            Assert.That(iniFile.Section("Foo").Get("bar"), Is.Not.Null);
+            iniFile.Section("Foo").Set("bar", "   ");
+            Assert.That(iniFile.Section("Foo").Get("bar"), Is.Null);
+
+            Assert.That(iniFile.Section("Foo2").Get("no"), Is.Not.Null);
+            iniFile.Section("Foo2").RemoveProperty("no");
+            Assert.That(iniFile.Section("Foo2").Get("no"), Is.Null);
+        }
+
+        [Test]
+        public void TestSave()
+        {
+            var tempFilename = Path.GetTempFileName();
+            var iniFile = new IniFile();
+            iniFile.Section("Foo").Comment = "This is foo";
+            iniFile.Section("Foo").Set("bar", "1");
+            iniFile.Section("Foo").Set("baz", "qux", comment: "bazy baz");
+            iniFile.Save(tempFilename);
+
+            Assert.That(File.ReadAllText(tempFilename), Is.EqualTo(";This is foo\r\n[Foo]\r\nbar=1\r\n;bazy baz\r\nbaz=qux\r\n\r\n"));
+        }
+
+        [Test]
+        public void TestPadding()
+        {
+            var tempFilename = Path.GetTempFileName();
+            var iniFile = new IniFile {WriteSpacingBetweenNameAndValue = true};
+            iniFile.Section("Foo").Set("bar", "1");
+            iniFile.Save(tempFilename);
+
+            Assert.That(File.ReadAllText(tempFilename), Is.EqualTo("[Foo]\r\nbar = 1\r\n\r\n"));
+        }
+
+        [Test]
+        public void TestDoNotWriteSectionIfEmpty()
+        {
+            var tempFilename = Path.GetTempFileName();
+            var iniFile = new IniFile();
+            iniFile.Section("Foo").Set("bar", "1");
+            iniFile.Section("Foo2");
+            iniFile.Save(tempFilename);
+
+            Assert.That(File.ReadAllText(tempFilename), Is.EqualTo("[Foo]\r\nbar=1\r\n\r\n"));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,48 @@
+﻿
+# IniFile
+
+IniFile is a single C# file that offers reading from and writing to INI files.
+You can drop it straight into your project without adding any DLL references.
+
+## Basic usage
+
+### Reading an INI file
+
+```csharp
+// [foo]
+// bar=1
+// baz=qux
+// [foo2]
+// bar=2
+		
+var iniFile = new IniFile("foo.ini");
+iniFile.Section("foo").Get("bar"); // "1"
+iniFile.Section("foo").Get("baz"); // "qux"
+		
+foreach (var section in iniFile.Sections())
+	Console.WriteLine(section.Name); // foo
+								     // bar
+```
+
+### Writing an INI file	
+
+```csharp
+var iniFile = new IniFile();
+	
+iniFile.Section("Foo").Comment = "This is foo";
+iniFile.Section("Foo").Set("bar", "1");
+iniFile.Section("Foo").Set("baz", "qux", comment: "baz is qux");
+
+iniFile.Section("Foo2").Set("bar", "2");
+
+iniFile.Save("foo.ini");
+
+// ;This is foo
+// [foo]
+// bar=1
+// ;baz is qux
+// baz=qux
+// [foo2]
+// bar=2
+```
+
